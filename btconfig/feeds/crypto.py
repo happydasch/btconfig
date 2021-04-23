a222701ae9e942b482ccc3fc2258f99070c8ee0c
Daniel Schindler <daniel@vcard24.de>
2021-04-23 16:38:39 +0200

    removed wrong constants

--- btconfig/feeds/crypto.py
+++ btconfig/feeds/crypto.py
@@ -44,22 +44,6 @@ class BinanceDownload(btconfig.BTConfigDataloader):
         (bt.TimeFrame.Months, 1): '1M'
     }
 
-    KLINE_INTERVAL_1MINUTE = '1m'
-    KLINE_INTERVAL_3MINUTE = '3m'
-    KLINE_INTERVAL_5MINUTE = '5m'
-    KLINE_INTERVAL_15MINUTE = '15m'
-    KLINE_INTERVAL_30MINUTE = '30m'
-    KLINE_INTERVAL_1HOUR = '1h'
-    KLINE_INTERVAL_2HOUR = '2h'
-    KLINE_INTERVAL_4HOUR = '4h'
-    KLINE_INTERVAL_6HOUR = '6h'
-    KLINE_INTERVAL_8HOUR = '8h'
-    KLINE_INTERVAL_12HOUR = '12h'
-    KLINE_INTERVAL_1DAY = '1d'
-    KLINE_INTERVAL_3DAY = '3d'
-    KLINE_INTERVAL_1WEEK = '1w'
-    KLINE_INTERVAL_1MONTH = '1M'
-
     def prepare(self):
         self.api_key = self._cfg.get('binance_api_key', '')
         self.api_secret = self._cfg.get('binance_api_secret', '')
