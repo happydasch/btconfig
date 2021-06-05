93cb51d7df0a794dd76e6433e989e70d723f196e
Daniel Schindler <daniel@vcard24.de>
2021-06-05 22:39:52 +0200

    fixed tz related dt offset issue

--- btconfig/feeds/csv.py
+++ btconfig/feeds/csv.py
@@ -2,7 +2,7 @@ from __future__ import division, absolute_import, print_function
 
 import backtrader as bt
 
-from datetime import timedelta
+from datetime import timedelta, timezone
 from backtrader.utils import date2num
 from btconfig.helper import get_starttime
 
@@ -17,19 +17,19 @@ class CSVAdjustTime(bt.feeds.GenericCSVData):
 
     def _loadline(self, linetokens):
         res = super(CSVAdjustTime, self)._loadline(linetokens)
+        # ensure that utc time is being used
+        dt = self.datetime.datetime(0, tz=timezone.utc)
         if self.p.adjstarttime:
             # move time to start time of next candle
             # and subtract 0.1 miliseconds (ensures no
             # rounding issues, 10 microseconds is minimum)
             new_date = get_starttime(
-                self._timeframe,
-                self._compression,
-                self.datetime.datetime(0),
-                self.p.sessionstart,
-                -1) - timedelta(microseconds=100)
+                self._timeframe, self._compression, dt,
+                self.p.sessionstart, -1)
+            new_date -= timedelta(microseconds=100)
             self.datetime[0] = date2num(new_date)
         else:
-            self.datetime[0] = date2num(self.datetime.datetime(0))
+            self.datetime[0] = date2num(dt)
         return res
 
 
