0a9d547c036b7929a1725a4fe9baaee68961d721
happydasch <daniel@vcard24.de>
2022-04-04 09:12:10 +0200

    added helper function to parse funding rates from coinglass

--- btconfig/utils/api/coinglass.py
+++ btconfig/utils/api/coinglass.py
@@ -2,6 +2,8 @@ from __future__ import division, absolute_import, print_function
 
 from btconfig import BTConfigApiClient
 
+import pandas as pd
+
 
 class CoinGlassClient(BTConfigApiClient):
     '''
@@ -25,3 +27,17 @@ class CoinGlassClient(BTConfigApiClient):
         }
         rates = self._request(path, exceptions=True, json=True, **kwargs)
         return rates['data']
+
+
+def create_funding_rates_df(data):
+    df = pd.DataFrame()
+    df['datetime'] = pd.to_datetime(data['dateList'], unit='ms')
+    df['price'] = pd.to_numeric(data['priceList'])
+    for d, d_cols in [['data', 'dataMap'], ['fr_data', 'frDataMap']]:
+        cols = []
+        for col in data[d_cols]:
+            c_name = f'{d}_{col}'
+            cols.append(c_name)
+            df[c_name] = pd.Series(pd.to_numeric(data[d_cols][col]))
+        df[d] = df[cols].mean(axis=1)
+    return df
