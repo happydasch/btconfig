e38e3a8ac49c29dee29901d83dae663119a1fb47
happydasch <daniel@vcard24.de>
2022-05-02 09:40:11 +0200

    fixed typo

--- btconfig/utils/dataloader/ftx.py
+++ btconfig/utils/dataloader/ftx.py
@@ -65,7 +65,7 @@ class FTXDataloaderApp:
         fromdate_ts = fromdate.timestamp()
         todate_ts = None
         if todate:
-            todate_ts = todate.timesamp()
+            todate_ts = todate.timestamp()
         data = self.client.getAllFundingRates(fromdate=fromdate_ts, todate=todate_ts)
         data_df = create_funding_rates_df(data)
         return data_df
@@ -83,7 +83,7 @@ class FTXDataloaderApp:
             fromdate_ts = fromdate.timestamp()
         todate_ts = None
         if todate:
-            todate_ts = todate.timesamp()
+            todate_ts = todate.timestamp()
         data = self.client.getFundingRates(future, fromdate_ts, todate_ts)
         data_df = create_funding_rates_df(data)
         return data_df
