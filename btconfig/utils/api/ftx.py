3c64b5dc83c5da659b08c5f62bc44005ed94a537
Jens Halsberghe <jens_halsberghe@hotmail.com>
2022-05-12 20:52:07 +0100

    Update ftx.py

--- btconfig/utils/api/ftx.py
+++ btconfig/utils/api/ftx.py
@@ -8,11 +8,12 @@ import pandas as pd
 
 
 class FTXClient(BTConfigApiClient):
-    '''
+    """
     FTX Client
 
     https://docs.ftx.com/#overview
-    '''
+    """
+
     RESOLUTIONS = {
         (bt.TimeFrame.Seconds, 15): 15,
         (bt.TimeFrame.Minutes, 1): 60,
@@ -24,13 +25,12 @@ class FTXClient(BTConfigApiClient):
         (bt.TimeFrame.Days, 1): 86400,
     }
 
-    def __init__(self, api_key='', api_secret='', **kwargs):
+    def __init__(self, api_key="", api_secret="", **kwargs):
         self.api_key = api_key
         self.api_secret = api_secret
         super(FTXClient, self).__init__(
-            base_url='https://ftx.com/api/',
-            headers={},
-            **kwargs)
+            base_url="https://ftx.com/api/", headers={}, **kwargs
+        )
 
     def _requestUrl(self, url):
         # FIXME auth details can be added here
@@ -39,47 +39,43 @@ class FTXClient(BTConfigApiClient):
 
     def getMarkets(self):
         # https://docs.ftx.com/#get-markets
-        path = 'markets'
+        path = "markets"
         response = self._request(path, json=True)
-        return response['result']
+        return response["result"]
 
-    def getMarketCandles(
-            self, symbol, start_time=None, end_time=None, resolution=3600):
+    def getMarketCandles(self, symbol, start_time=None, end_time=None, resolution=3600):
         # https://docs.ftx.com/#get-historical-prices
-        path = f'markets/{symbol}/candles'
-        kwargs = {'resolution': resolution}
+        path = f"markets/{symbol}/candles"
+        kwargs = {"resolution": resolution}
         if start_time:
-            kwargs['start_time'] = int(start_time)
+            kwargs["start_time"] = int(start_time)
             if not end_time:
                 end_time = int(datetime.utcnow().timestamp())
         if end_time:
-            kwargs['end_time'] = int(end_time)
+            kwargs["end_time"] = int(end_time)
         res = []
 
         while True:
             response = self._request(path, **kwargs)
             if response.status_code != 200:
-                raise Exception(f'{response.url}: {response.text}')
+                raise Exception(f"{response.url}: {response.text}")
             tmp = response.json()
-            if not len(tmp['result']):
+            if not len(tmp["result"]):
                 break
             if not len(res):
-                res = tmp['result']
+                res = tmp["result"]
             else:
                 # if already fetched something skip first entry
                 # to prevent double entries
-                res = tmp['result'][:-1] + res
-            last_dt = int(tmp['result'][0]['time'] / 1000)
-            if (kwargs.get('start_time', 0)
-                    and kwargs.get('start_time', 0) >= last_dt):
+                res = tmp["result"][:-1] + res
+            last_dt = int(tmp["result"][0]["time"] / 1000)
+            if kwargs.get("start_time", 0) and kwargs.get("start_time", 0) >= last_dt:
                 break
-            if (kwargs.get('end_time', 0)
-                    and kwargs.get('end_time', 0) <= last_dt):
+            if kwargs.get("end_time", 0) and kwargs.get("end_time", 0) <= last_dt:
                 break
-            if (not kwargs.get('start_time')
-                    and not kwargs.get('end_time')):
+            if not kwargs.get("start_time") and not kwargs.get("end_time"):
                 break
-            kwargs['end_time'] = last_dt
+            kwargs["end_time"] = last_dt
         return res
 
     def getAllFundingRates(self, start_time=None, end_time=None):
@@ -88,45 +84,50 @@ class FTXClient(BTConfigApiClient):
 
     def getFundingRates(self, future=None, start_time=None, end_time=None):
         # https://docs.ftx.com/#get-funding-rates
-        path = 'funding_rates'
+        path = "funding_rates"
         kwargs = {}
         if future:
-            kwargs['future'] = future
+            kwargs["future"] = future
         if end_time:
-            kwargs['end_time'] = int(end_time)
+            kwargs["end_time"] = int(end_time)
         if start_time:
-            kwargs['start_time'] = int(start_time)
+            kwargs["start_time"] = int(start_time)
         res = []
         while True:
             response = self._request(path, **kwargs)
             if response.status_code != 200:
-                raise Exception(f'{response.url}: {response.text}')
+                raise Exception(f"{response.url}: {response.text}")
             tmp = response.json()
-            if not len(tmp['result']):
+            if not len(tmp["result"]):
                 break
             if not len(res):
-                res = tmp['result']
+                res = tmp["result"]
             else:
                 # if already fetched something skip first entry
                 # to prevent double entries
-                res += tmp['result'][1:]
-            last_dt = int(datetime.fromisoformat(
-                tmp['result'][-1]['time']).timestamp())
-            if (kwargs.get('start_time', 0)
-                    and kwargs.get('start_time', 0) >= last_dt):
+                res += tmp["result"][1:]
+            last_dt = int(datetime.fromisoformat(tmp["result"][-1]["time"]).timestamp())
+            if kwargs.get("start_time", 0) and kwargs.get("start_time", 0) >= last_dt:
                 break
-            if (kwargs.get('end_time', 0)
-                    and kwargs.get('end_time', 0) <= last_dt):
+            if kwargs.get("end_time", 0) and kwargs.get("end_time", 0) <= last_dt:
                 break
-            if (not kwargs.get('start_time')
-                    and not kwargs.get('end_time')):
+            if not kwargs.get("start_time") and not kwargs.get("end_time"):
                 break
-            kwargs['end_time'] = last_dt
+            kwargs["end_time"] = last_dt
         return res
+
+    def listFuturesInfo(self):
+        """
+        https://docs.ftx.com/#futures
+        retrieve info for all avaialable futures
+        """
+        path = "futures"
+        response = self._request(path, json=True)
+        return response["result"]
 
 
 def create_data_df(data):
-    '''
+    """
     Market Data
 
     Creates and returns a DataFrame created from data (returned from api)
@@ -138,19 +139,19 @@ def create_data_df(data):
         {'startTime': '2022-03-26T23:00:00+00:00', 'time': 1648335600000.0, 'open': 10.516, 'high': 10.654, 'low': 10.516, 'close': 10.634, 'volume': 884336.7059},
         ...
     ]
-    '''
+    """
     if data is None:
         return
     res = pd.DataFrame(data)
-    res['time'] = pd.to_datetime(res['time'], unit='ms')
-    res.rename(columns={'time': 'datetime'}, inplace=True)
-    res.drop(columns=['startTime'], inplace=True)
-    res['datetime'] = res['datetime'].dt.tz_localize(None)
+    res["time"] = pd.to_datetime(res["time"], unit="ms")
+    res.rename(columns={"time": "datetime"}, inplace=True)
+    res.drop(columns=["startTime"], inplace=True)
+    res["datetime"] = res["datetime"].dt.tz_localize(None)
     return res
 
 
 def create_funding_rates_df(data):
-    '''
+    """
     Funding Rates
 
     Creates and returns a DataFrame created from data (returned from api)
@@ -162,11 +163,28 @@ def create_funding_rates_df(data):
         {'future': 'UNI-PERP', 'rate': 3e-06, 'time': '2022-04-05T12:00:00+00:00'},
         ...
     ]
-    '''
+    """
     if data is None:
         return
     res = pd.DataFrame(data)
-    res['time'] = pd.to_datetime(res['time'])
-    res.rename(columns={'time': 'datetime', 'rate': 'close'}, inplace=True)
-    res['datetime'] = res['datetime'].dt.tz_localize(None)
-    return res[['datetime', 'close', 'future']].sort_values(by='datetime')
+    res["time"] = pd.to_datetime(res["time"])
+    res.rename(columns={"time": "datetime", "rate": "close"}, inplace=True)
+    res["datetime"] = res["datetime"].dt.tz_localize(None)
+    return res[["datetime", "close", "future"]].sort_values(by="datetime")
+
+
+def listFuturesdf(data, type=None) -> pd.DataFrame:
+    """
+    Returns a df of all the available futures info
+    types: perpetual, future, prediction, move
+    """
+    if data is None:
+        return
+
+    df = pd.DataFrame.from_dict(data)
+    df.set_index("name", inplace=True)
+
+    if type is not None:
+        df = df[df["type"] == type]
+
+    return df
