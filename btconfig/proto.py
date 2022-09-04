3929ea709129762d08428988e3041510491119dc
happydasch <daniel@vcard24.de>
2022-09-04 14:00:53 +0200

    added pip location calculation to proto

--- btconfig/proto.py
+++ btconfig/proto.py
@@ -286,6 +286,50 @@ class ForexProtoStrategy(ProtoStrategy):
 
         super(ForexProtoStrategy, self).__init__()
 
+    def pip_location(self, value, to_one=True):
+        '''
+        Returns the pip location for given value
+
+        Ex.
+        value=0 - pip location=0
+        value=1 - pip location=0
+        value=5 - pip location=1 (to_one = True)
+        value=5 - pip location=0 (to_one = False)
+        value=10 - pip location=1
+        value=25 - pip location=2 (to_one = True)
+        value=25 - pip location=1 (to_one = False)
+        value=0.5 - pip location=0 (to_one = True)
+        value=0.5 - pip location=-1 (to_one = False)
+        value=0.25 - pip location=0 (to_one = True)
+        value=0.25 - pip location=-1 (to_one = False)
+        value=0.1 - pip location=-1
+        value=0.05 - pip location=-1 (to_one = True)
+        value=0.05 - pip location=-2 (to_one = False)
+        value=0.01 - pip location=-2
+        value=0.001 - pip location=-3
+        '''
+        pip_location = 0
+        if value == 0:
+            return pip_location
+        while True:
+            mult = float(10 ** -pip_location)
+            pips = value * mult
+            if value >= 1:
+                if pips <= 1:
+                    if pips < 1 and not to_one:
+                        pip_location -= 1
+                    break
+                pip_location += 1
+            else:
+                if pips > 1:
+                    if to_one:
+                        pip_location += 1
+                    break
+                elif pips == 1:
+                    break
+                pip_location -= 1
+        return pip_location
+
     def pips_from_value(self, value, pip_precision=0, pip_location=None):
         '''
         Returns pips from a price value
