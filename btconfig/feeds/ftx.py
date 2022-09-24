72aa51f99c6d520370ba1aff7658e49af95db6c0
happydasch <daniel@vcard24.de>
2022-09-24 11:14:34 +0200

    fixed ftx dt issues added new base class for ftx, so the adjusted dt works with backtrader

--- btconfig/feeds/ftx.py
+++ btconfig/feeds/ftx.py
@@ -16,10 +16,9 @@ from datetime import datetime, timezone, timedelta
 from typing import DefaultDict, List, Dict, Callable
 
 from btconfig import BTConfigDataloader
-from btconfig.helper import get_data_dates, get_starttime
+from btconfig.helper import get_data_dates, get_starttime, parse_dt
 
-from btconfig.feeds.misc import (
-    CSVAdjustTime, CSVAdjustTimeCloseOnly)
+from btconfig.feeds.misc import CSVAdjustTime
 from btconfig.utils.api.ftx import create_data_df
 from btconfig.utils.dataloader import FTXDataloaderApp
 from btconfig.utils.websocket import WebsocketManager
@@ -210,7 +209,7 @@ class FTXFundingRatesDataloader(BTConfigDataloader):
     PREFIX = 'FTX_FR'
 
     def _prepare(self):
-        self._cls = CSVAdjustTimeCloseOnly
+        self._cls = FTXFundingRates
         api_key = self._cfg.get('api_key', '')
         api_secret = self._cfg.get('api_secret', '')
         pause = self._cfg.get('pause', 0.0285714)
@@ -264,7 +263,7 @@ class FTXFundingRatesDataloaderLive(FTXFundingRatesDataloader):
         if hasattr(self._instance, 'ftx_loader'):
             data.ftx_loader = self._instance.ftx_loader
         else:
-             data.ftx_loader = self.loader
+            data.ftx_loader = self.loader
         return data
 
 
@@ -589,7 +588,42 @@ class FTXDataLive(CSVAdjustTime):
         return True
 
 
-class FTXFundingRatesLive(CSVAdjustTimeCloseOnly):
+class FTXFundingRates(bt.feeds.GenericCSVData):
+
+    params = dict(
+        adjstarttime=False,
+        roundvalues=False,
+        roundprecision=8,
+        dtformat=parse_dt,
+        datetime=0, open=1, high=1, low=1, close=1,
+        volume=-1, openinterest=-1
+    )
+
+    def _loadline(self, linetokens):
+        res = super(FTXFundingRates, self)._loadline(linetokens)
+        # if values should be rounded, all lines will get rounded
+        if self.p.roundvalues:
+            for linefield in (x for x in self.getlinealiases() if x != 'datetime'):
+                csvidx = getattr(self.params, linefield)
+                if csvidx is None or csvidx < 0:
+                    continue
+                line = getattr(self.lines, linefield)
+                if line[0] == line[0]:
+                    line[0] = round(line[0], self.p.roundprecision)
+        # ensure that utc time is being used
+        dt = self.datetime.datetime(0, tz=timezone.utc)
+        if self.p.adjstarttime:
+            # move time to end time of prev candle
+            # subtract 0.1 miliseconds (ensures no
+            # rounding issues, 10 microseconds is minimum)
+            new_date = dt - timedelta(microseconds=100)
+            self.datetime[0] = bt.date2num(new_date)
+        else:
+            self.datetime[0] = bt.date2num(dt)
+        return res
+
+
+class FTXFundingRatesLive(FTXFundingRates):
 
     # Extends ftx funding rates with live data
 
